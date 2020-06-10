27a12eea6f72543c4297efd1282e5d56f4626dd3
royeradames <royeraadames@gmail.com>
2020-06-10 15:49:06 -0400

    made a function that copies the original flavors'

--- index.js
+++ index.js
@@ -147,11 +147,13 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/){
-
+function copy(arr){
+    let copyArr = [...arr];
     /*code here*/
-
+    return copyArr;
 }
+const copyOriginalFlavors = copy(originalFlavors);
+console.log(`Copy of the array: ${copyOriginalFlavors}`);
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
@@ -168,14 +170,13 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
+function filterByWord(arr, ){
 
     /*code here*/
 
 }
 
-
-
+console.log(filterByWord(originalFlavors, "chocolate"));
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/ 
 
 /* STRETCH 1: Write a function that returns the average number of words in an array. You should be able to use this function for any array, but can test with originalFlavors.
